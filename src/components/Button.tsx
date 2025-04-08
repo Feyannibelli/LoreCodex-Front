8b4e39ed7658f90fe176a97cfd72f4a77b72ae5d
Feyannibelli <fyann06@gmail.com>
2025-04-07 22:23:10 -0300

    added component buttons

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../css/Button.css'
+
+interface ButtonProps {
+    onClick?: () => void
+    children: React.ReactNode
+    type?: 'button' | 'submit' | 'reset'
+    className?: string
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, children, type = 'button', className }) => {
+    return (
+        <button type={type} onClick={onClick} className={`custom-button ${className || ''}`}>
+            {children}
+        </button>
+    )
+}
+
+export default Button
